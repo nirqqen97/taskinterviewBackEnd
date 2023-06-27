a300cad24a43cc3247eff95830609a462930208d
nirqqen97 <nirqqen97@gmail.com>
2023-06-27 18:06:01 +0200

    add CORS

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import router from './router.js'
-const PORT = 5000
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './router.js';
+import cors from 'cors';
 
-const URL = "mongodb+srv://user:user@cluster0.ul2p0ce.mongodb.net/"
+const PORT = 5000;
+const URL = "mongodb+srv://user:user@cluster0.ul2p0ce.mongodb.net/";
+const app = express();
 
+app.use(express.json());
+app.use(cors());
+app.use("/streamers", router);
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URL);
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const app = express()
-
-app.use(express.json())
-app.use("/streamers", router)
-
-const connectDB = async () => { 
-    try {
-        await mongoose.connect(URL)
-    } catch (error) {
-        console.log(error)
-    }
-}
 async function startApp() {
-    try {
-        app.listen(PORT, (() => {
-        console.log("server started")
-}))
-        
-    } catch (e) {
-       console.log(e) 
-    }
-    
+  try {
+    app.listen(PORT, () => {
+      console.log("Server started on port", PORT);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 }
 
-connectDB().then(startApp())
-
+connectDB().then(startApp());
