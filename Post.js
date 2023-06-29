8628eb8888c83271d33f88d22ddd7effe3b4adcc
nirqqen97 <nirqqen97@gmail.com>
2023-06-29 13:37:54 +0200

    add enum

--- Post.js
+++ Post.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const Post = mongoose.Schema( { 
     author : {type : String, required : true},
     description : {type : String, required : true},
-    platform : {type : String, required : true},
+    platform : {type : String, required : true,  enum: ['Twitch', 'YouTube', 'TikTok','Kick','Rumble']},
     upvote : {type : Number, default : 0 },
     downvote : {type : Number, default : 0},
     picture : {type : String, required : true}
